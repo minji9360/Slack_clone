60d3ea8ea937905b1d1309ed9506f675d5314664
jtbc <jtbc@naver.com>
2021-03-19 07:53:24 +0900

    refactor: channels 하위 button div로 감싸기
    - channels 버튼 클릭 시 하위 메뉴가 보이지 않아야 하므로 전체 div로 감쌈
    - 구현 후에 메뉴를 li로 refactor 해야할 듯

--- click.js
+++ click.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelector(".menu");
+const channels = document.querySelector(".channels");
 
 function clickMenu(button) {
 	// menu 버튼 전체 클릭 상태 비활성화로 변경
@@ -12,15 +13,16 @@ function clickMenu(button) {
 		}
 	}
 
-	// 클릭한 버튼이 이전에 클릭한 적 없을 경우 클릭 상태 활성화
-	if (!button.classList.contains("click")) {
-		if (button.children[0].classList.contains("menu__img")) {
-			// 메뉴 아이콘이 있을 경우
-			button.children[0].classList.add("hidden");
-			button.children[1].classList.remove("hidden");
-		}
-		button.classList.add("click");
+	for (i = 0; i < channels.childElementCount; i++)
+		channels.children[i].classList.remove("click");
+
+	// 클릭한 버튼 활성화
+	if (button.children[0].classList.contains("menu__img")) {
+		// 메뉴 아이콘이 있을 경우
+		button.children[0].classList.add("hidden");
+		button.children[1].classList.remove("hidden");
 	}
+	button.classList.add("click");
 }
 
 function init() {}
