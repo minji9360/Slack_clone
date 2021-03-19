4fb344c1bf3faacdf59ef984f1622bd9ebb1093b
jtbc <jtbc@naver.com>
2021-03-19 21:25:10 +0900

    feat: 슬라이드 버튼 클릭 기능 추가
    - 채널과 다이렉트 버튼 클릭 시 하위 메뉴들이 slide 되도록 하는
      clickMenuSlide 함수 추가
    - 메뉴 클릭 시 채널이나 다이렉트 버튼이면 버튼 이미지(삼각형)이
      회전하도록 수정

--- click.js
+++ click.js
@@ -1,5 +1,44 @@
 const buttons = document.querySelector(".menu");
 const channels = document.querySelector(".channels");
+const direct = document.querySelector(".direct__button");
+
+function clickMenuSlide(button) {
+	if (button.classList.contains("channel")) {
+		// 클릭한 버튼이 channel일 경우
+		if (button.classList.contains("slide")) {
+			// 해당 버튼을 재클릭 하는 경우
+			console.log(button);
+			button.classList.remove("slide");
+			button.children[0].classList.remove("close");
+			for (i = 0; i < channels.childElementCount; i++) {
+				channels.children[i].classList.remove("hidden");
+			}
+		} else {
+			// 해당 버튼을 처음 클릭하는 경우
+			button.classList.add("slide");
+			button.children[0].classList.add("close");
+			button.classList.add("slide");
+			for (i = 0; i < channels.childElementCount; i++) {
+				if (!channels.children[i].classList.contains("click")) {
+					channels.children[i].classList.add("hidden");
+				}
+			}
+		}
+	} else {
+		// 클릭한 버튼이 다이렉트일 경우
+		if (button.classList.contains("slide")) {
+			button.classList.remove("slide");
+			button.children[0].classList.remove("close");
+			direct.classList.remove("hidden");
+		} else {
+			if (!direct.classList.contains("click")) {
+				button.classList.add("slide");
+				button.children[0].classList.add("close");
+				direct.classList.add("hidden");
+			}
+		}
+	}
+}
 
 function clickMenu(button) {
 	// menu 버튼 전체 클릭 상태 비활성화로 변경
@@ -8,8 +47,13 @@ function clickMenu(button) {
 			buttons.children[i].classList.remove("click");
 		}
 		if (buttons.children[i].children[0].classList.contains("menu__img")) {
-			buttons.children[i].children[0].classList.remove("hidden");
-			buttons.children[i].children[1].classList.add("hidden");
+			if (
+				!buttons.children[i].classList.contains("channel") &
+				!buttons.children[i].classList.contains("direct")
+			) {
+				buttons.children[i].children[0].classList.remove("hidden");
+				buttons.children[i].children[1].classList.add("hidden");
+			}
 		}
 	}
 
